f38054114abfa79dd3cddb8f24554f162e2ce847
JalenCameron <109976308+JalenCameron@users.noreply.github.com>
2022-10-07 12:24:01 -0700

    All board pieces added and functions created

--- src/Board.js
+++ src/Board.js
@@ -54,5 +54,75 @@ const Board = ({ reset, setReset, winner, setWinner }) => {
     setReset(false);
   }, [reset, setReset, setWinner]);
 
+  // Using the useEffect Hook to check for a winner every round
+  useEffect(() => {
+    // Condition to check for the win in each row
+    const checkRow = () => {
+        let answer = false;
+        for (let i = 0; i < 9; i += 3) {
+            answer |= (data[i] === data[i + 1] &&
+            data[i] === data[i + 2] &&
+            data[i] !== '')
+        };
+        return answer;
+    }
 
+    // Condition to check for the win in each column
+    const checkColumn = () => {
+        let answer = false;
+        for (let i = 0; i < 3; i++) {
+            answer |= data[i] === data[i + 3] &&
+            data[i] === data[i + 6] &&
+            data[i] !== "";
+        }
+        return answer;
+    };
+
+    // Condition to check for the win if it's diagonal
+    const checkDiagonal = () => {
+        return (
+          (data[0] === data[4] && data[0] === data[8] && data[0] !== "") ||
+          (data[2] === data[4] && data[2] === data[6] && data[2] !== "")
+        );
+    };
+
+    // Condition to check if there's any win at all using the functions created
+    const checkWin = () => {
+        return (checkRow() || checkColumn() || checkDiagonal());
+    };
+
+    // Condition to check for a tie
+    const checkTie = () => {
+        let count = 0;
+        data.forEach((cell) => {
+            if (cell != '') {
+                count++;
+            }
+        })
+        return count === 9;
+    };
+
+    // Setting the winner if there's a win
+    if (checkWin()) {
+        setWinner(turn === 0 ? "Player 2 Wins!" : "Player 1 Wins");
+    } else if (checkTie()) {
+        setWinner("It's a Tie!");
+    };
+  })
+  
+  return (
+    <div ref={boardReference} className="board">
+      <div className="input input-1" onClick={(e) => draw(e, 1)}></div>
+      <div className="input input-2" onClick={(e) => draw(e, 2)}></div>
+      <div className="input input-3" onClick={(e) => draw(e, 3)}></div>
+      <div className="input input-4" onClick={(e) => draw(e, 4)}></div>
+      <div className="input input-5" onClick={(e) => draw(e, 5)}></div>
+      <div className="input input-6" onClick={(e) => draw(e, 6)}></div>
+      <div className="input input-7" onClick={(e) => draw(e, 7)}></div>
+      <div className="input input-8" onClick={(e) => draw(e, 8)}></div>
+      <div className="input input-9" onClick={(e) => draw(e, 9)}></div>
+    </div>
+  );
 };
+
+export default Board;
